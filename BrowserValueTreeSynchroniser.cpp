6442a100c01ff2be006963cf70ff16618c2dac03
Tom Duncalf <tom@tomduncalf.com>
2021-05-23 16:00:55 +0100

    WIP batching changes

--- BrowserValueTreeSynchroniser.cpp
+++ BrowserValueTreeSynchroniser.cpp
@@ -2,6 +2,9 @@ namespace tomduncalf
 {
 namespace BrowserIntegration
 {
+
+#define VALUE_TREE_SYNCHRONISER_BATCH_CHANGES 0
+
     BrowserValueTreeSynchroniser::BrowserValueTreeSynchroniser (juce::ValueTree& vt, juce::Identifier id, BrowserIntegration& b)
         : juce::ValueTreeSynchroniser (vt),
           treeId (id),
@@ -11,13 +14,35 @@ namespace BrowserIntegration
 
     void BrowserValueTreeSynchroniser::stateChanged (const void* encodedChange, size_t encodedChangeSize)
     {
+        // TODO could filter out redundant multiple changes to a single parameter
+        // in a single batch here or elsewhere
+        auto change = juce::Base64::toBase64 (encodedChange, encodedChangeSize);
+        queuedChanges.add (change);
+        
+    #if VALUE_TREE_SYNCHRONISER_BATCH_CHANGES
+        triggerAsyncUpdate();
+    #else
+        flushUpdates();
+    #endif
+    }
+
+
+void BrowserValueTreeSynchroniser::handleAsyncUpdate()
+{
+    flushUpdates();
+}
+
+void BrowserValueTreeSynchroniser::flushUpdates()
+{
+    if (queuedChanges.size() > 0)
+    {
         auto* dataObj = new juce::DynamicObject();
         dataObj->setProperty ("treeId", treeId.toString());
-
-        auto change = juce::Base64::toBase64 (encodedChange, encodedChangeSize);
-        dataObj->setProperty ("change", change);
-
+        dataObj->setProperty ("changes", queuedChanges);
         browserIntegration.sendEventToBrowser ("valueTreeStateChange", dataObj);
+        
+        queuedChanges.clear();
     }
+}
 }// namespace BrowserIntegration
 }// namespace tomduncalf
--- BrowserValueTreeSynchroniser.h
+++ BrowserValueTreeSynchroniser.h
@@ -9,15 +9,18 @@ namespace tomduncalf
 {
 namespace BrowserIntegration
 {
-    class BrowserValueTreeSynchroniser : public juce::ValueTreeSynchroniser
+    class BrowserValueTreeSynchroniser : public juce::ValueTreeSynchroniser, private juce::AsyncUpdater
     {
     public:
         BrowserValueTreeSynchroniser (juce::ValueTree& vt, juce::Identifier id, BrowserIntegration& b);
 
         void stateChanged (const void* encodedChange, size_t encodedChangeSize) override;
+        void handleAsyncUpdate() override;
+        void flushUpdates();
 
     protected:
         juce::Identifier treeId;
+        juce::StringArray queuedChanges;
         BrowserIntegration& browserIntegration;
     };
 }// namespace BrowserIntegration
