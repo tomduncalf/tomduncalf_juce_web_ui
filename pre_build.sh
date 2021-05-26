5fa902ee5a6b1eb3ef9a239e5f1dda7a687fcf69
Tom Duncalf <tom@tomduncalf.com>
2021-05-26 09:54:32 +0100

    Move dev server script to pre_build.sh and make it build in release mode

--- pre_build.sh
+++ pre_build.sh
@@ -1,6 +1,6 @@
 # Only start the dev server in debug mode, and for the "Shared Code" target of the build
 # (Xcode will try to run the script twice, once for the Shared Code and once for the actual build)
-if [ "$CONFIGURATION" == "Debug" ] &&  [[ $TARGET_NAME == *" - Shared Code" ]]; then
+if [ "$CONFIGURATION" == "Debug" ] && [[ $TARGET_NAME == *" - Shared Code" ]]; then
   for i in "${TARGET_BUILD_DIR}/../../"*.plist; do /usr/libexec/PlistBuddy -c "Set :DevServerIP `ipconfig getifaddr en0`" "$i"; done
 
   if nc -w 5 -z localhost 3000 ; then
@@ -15,4 +15,10 @@ do script "cd \"${SRCROOT}/../../ui\"; BROWSER=none yarn start"
 end tell
 END
   fi
+elif [ "$CONFIGURATION" == "Release" ] && [[ $TARGET_NAME == *" - Shared Code" ]]; then
+        osascript <<END
+tell application "Terminal"
+do script "cd \"${SRCROOT}/../../ui\"; BROWSER=none yarn build"
+end tell
+END
 fi
